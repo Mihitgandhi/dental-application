ad10a3edb0b15bc76462b8c9cee4c50b94b9c51f
Yashvi Gandhi <yashvi21gandhi@gmail.com>
2023-12-01 20:53:01 +0530

    Book_Appointment and Contact_Us page

--- src/app/components/book-appoinment/book-appoinment.component.ts
+++ src/app/components/book-appoinment/book-appoinment.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book-appoinment',
   templateUrl: './book-appoinment.component.html',
-  styleUrl: './book-appoinment.component.scss'
+  styleUrl: './book-appoinment.component.scss',
 })
 export class BookAppoinmentComponent {
+  myName:string= "";
+  myEmail:string= "";
+  myTreatment:string= "";
+  myPhonenum: any= "";
+  myAddress:any="";
 
+  options=[
+    "Smile Design", 
+    "General consultation", 
+    "Bone Loss and Teeth Filling", 
+    "Teeth Cleaning and Whitening", 
+    "Gum Diseases and Bad Breath", 
+    "Toothache and Sensitive Teeth", 
+    "Kids Dentistry"
+  ]
+
+  selected ="Choose..."
+  
+  update(e:any){
+    this.selected = e.target.value
+  }
+
+  onClick(){
+    console.log("Name is :", this.myName),
+    console.log("Email is :", this.myEmail),
+    console.log("Treatment is :", this.myTreatment),
+    console.log("Phone is :", this.myPhonenum),
+    console.log("Phone is :", this.myAddress)
+  }
+  
+  
 }
