44792ea422e21f4aa22e11f5d4b387cad8bc780b
Yashvi Gandhi <yashvi21gandhi@gmail.com>
2024-04-03 21:52:35 +0530

    Home Page basic design

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  images=[
+    {name: '../../../assets/images/hd_home1.jpg', caption:'Clinic View'},
+    {name: '../../../assets/images/smile.jpg', caption:'Smile'},
+    {name: '../../../assets/images/implant.jpg', caption:'Implant Architecture'}
+  ];
 }
